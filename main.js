8d7dfe0818a7ab089e7cf42384e49842cb92d56c
Sulaiman-F <s.alfawzan18@gmail.com>
2025-05-08 09:59:11 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let text = document.getElementById("Text")
+let btn = document.getElementById("btn")
+function newtext() {
+    text.innerText = "HI"
+}
+function textbigger() {
+    text.style.fontSize = "2rem"
+
+}
+function textsmailer() {
+    text.style.fontSize = "0.5rem"
+}
+function textcolor() {
+    text.style.color = "blue"
+}
+function btncolor() {
+    btn.style.backgroundColor = "Blue"
+}
+
+function addimg() {
+    let img = document.getElementById("img")
+    img.src = "./image.png"
+}
+
